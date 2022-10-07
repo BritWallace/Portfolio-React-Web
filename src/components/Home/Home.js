1a69e73cfb6111cbfe1ccceec08f14fe34f3c35c
Brit Wallace <britwallace888@gmail.com>
2022-10-06 19:10:11 -0700

    add images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
+import classes from './Home.module.css'
+import b1 from './../../img/b1.jpeg'
 const Home = () => {
   return ( 
-    <div>
-      <h1>Home</h1>
+    <div className="classes.Home">
+      <div className="classes.Container">
+        <h1 className={classes.Hello}>Hello, world.</h1>
+        <h1>Welcome to my website</h1>
+      </div>
+      <img className={classes.b1} src={b1} alt="code"></img>
     </div>
     );
 }
